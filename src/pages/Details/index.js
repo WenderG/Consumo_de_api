db6cdddf0ec3b5535a76541e7b2defdf76704fe4
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-20 10:08:10 -0300

    :books: docs: Modificação

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,11 +1,47 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { APIKey } from '../../config/Key'
+import { Container } from './styles'
 
 function Details() {
 
     const { id } = useParams()
 
+    const [ movie, setMovie ] = useState({})
+    const image_path = 'https://image.tmdb.org/t/p/w500'
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US`)
+        .then(response => response.json())
+        .then(data => {
+
+            const {title, poster_path, overview, release_date} = data 
+
+            const movie = {
+                id,
+                title,
+                sinopse: overview,
+                image: `${image_path}${poster_path}`,
+                releaseDate: release_date,
+            }
+
+            setMovie(movie)
+        })
+
+    }, [id])
+
     return (
-        <h1>Página de detalhes</h1>
+        <Container>
+            <div className='movie'>
+                <img src={movie.image} alt={movie.sinopse}/>
+                <div className='datails'>
+                    <h1>{movie.title}</h1>
+                    <span>Sinopse: {movie.sinopse}</span>
+                    <span className='release-date'>Data de lançamento: {movie.releaseDate}</span>
+                    <Link to="/"><button>Voltar</button></Link>
+                </div>
+            </div>
+        </Container>
     )
 }
 
