4bd8e8444b6aaed83d96fc7871fd3c6f1823c3e3
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-21 09:13:11 -0300

    :sparkles: feat: Adicionando pasta components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="rodape">
+            &copy; 2023 - Desenvolvido por @WenderG
+        </div>
+    )
+}
+
+export default Footer
